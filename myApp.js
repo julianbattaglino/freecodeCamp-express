b8132ca5dc9426881e71ab453300ac03cfa461a5
Julian Battaglino <julianbattaglino@gmail.com>
2022-02-06 19:15:30 -0300

    .env

--- myApp.js
+++ myApp.js
@@ -1,7 +1,29 @@
 var express = require('express');
 var app = express();
+require('dotenv').config().
 
 console.log ("Hello World")
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
+  
+  app.use("/public", express.static(__dirname + "/public"));
+  
+
+app.get("/json", function(req, res) {
+    res.json(
+      {message: "Hello json"}
+    );
+  });
+  
+  var response = "Hello World".toUpperCase(); // now becomes "HELLO WORLD"
+  
+  if (process.env.MESSAGE_STYLE === "allCaps") {
+    response = "Hello World".toUpperCase();
+  } else {
+    response = "Hello World";
+  }
 
 
 
--- server.js
+++ server.js
@@ -21,27 +21,6 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
-app.use("/public", express.static(__dirname + "/public"));
-
-
-app.get("/json", function(req, res) {
-  res.json(
-    {message: "Hello json"}
-  );
-});
-
-var response = "Hello World".toUpperCase(); // now becomes "HELLO WORLD"
-
-if (process.env.MESSAGE_STYLE === "allCaps") {
-  response = "Hello World".toUpperCase();
-} else {
-  response = "Hello World";
-}
-
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')

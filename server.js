9555052362c24026bd5f0e72494c0ee00fb21028
Julian Battaglino <julianbattaglino@gmail.com>
2022-02-06 18:44:06 -0300

    serveCss

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use(express.static(__dirname + "/public/style.css"));
+app.use(express.static(__dirname + "/public"));
 
 
 var port = process.env.PORT || 3000;

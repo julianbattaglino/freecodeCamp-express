c320f7c6b118048e09e1936d6318bf6f1284eee9
Julian Battaglino <julianbattaglino@gmail.com>
2022-02-06 18:42:04 -0300

    expressStatic

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use(express.static(__dirname + "/public/style.css"));
+
 
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
